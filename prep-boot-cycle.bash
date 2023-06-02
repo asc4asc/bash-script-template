791ffe006d17c645ef8b86f7cc77d0b6e16a2af3
Andreas Schockenhoff <asc@ekf.de>
2023-06-02 10:15:17 +0200

    make nicer

--- prep-boot-cycle.bash
+++ prep-boot-cycle.bash
@@ -9,8 +9,14 @@ Helper script that prepare a new installed Ubuntu .... to work as a test system 
 
 The test system is a ubuntu 22.04 LTS. sudo must work.
 
+It set a automatic login with a terminal that execute the files in home autostart.dir.
+In autostart.dir we place a test4me.reboot file to automatic test the PC reboots. 
+Rename it to test4me.powercycle and a powercycle will be tested.
+
+Please add more test before this powercycle if you need.
+
 Usage:
-     -h|--help                  Displays this help
+     -?|-h|--help               Displays this help
      -v|--verbose               Displays verbose output
 EOF
 }
@@ -24,7 +30,7 @@ function parse_params() {
         param="$1"
         shift
         case $param in
-            \? | -h | --help)
+            -\? | -h | --help)
                 script_usage
                 exit 0
                 ;;
@@ -38,9 +44,9 @@ function parse_params() {
     done
 }
 
-# powercycle oder reboot oder mix am Namen fest machen.
-# * test4me-reboot
-# * test4me-powercycle
+# DESC: Make automatic sudo possible.
+# ARGS: None
+# OUTS: None
 readonly autostartdir="${HOME}/autostart.dir"
 function gen_sudo()
 {
@@ -48,13 +54,18 @@ function gen_sudo()
     sudo addgroup --group testsu && sudo adduser ${USER} testsu && echo '%testsu ALL=(ALL:ALL) NOPASSWD:ALL' > /tmp/gen4testing && sudo cp /tmp/gen4testing /etc/sudoers.d
 }
 
+# DESC: Place our testscript in the /use/local/bin dir ...
+# ARGS: None
+# OUTS: None
 function place_bin()
-# ~/autostart.dir/<link>named>test4me-powercycle to /usr/local/bin/test4me oder test4me-reboot
 {
     sudo cp ${1} /usr/local/bin/
     sudo chmod a+x /usr/local/bin/${1}
 }
 
+# DESC: Generate a autostart file in the user dir
+# ARGS: None
+# OUTS: None
 function gen_autoterm()
 {
     mkdir ${HOME}/.config/autostart
@@ -76,6 +87,9 @@ X-GNOME-Autostart-Delay=0
 EOF
 }
 
+# DESC: Generate a file that user login automatic
+# ARGS: None
+# OUTS: None
 function gen_login()
 {
     sudo cat << EOF  > /tmp/custom.conf
@@ -86,9 +100,12 @@ EOF
 sudo cp /tmp/custom.conf /etc/gdm3/custom.conf
 }
 
-
+# DESC: make a link to our testscript to execute it automatic at system start. 
+# ARGS: None
+# OUTS: None
 function make_autostart()
 {
+    place_bin test4me
     mkdir -p ${autostartdir}
     ln -s /usr/local/bin/test4me ${autostartdir}/test4me-reboot
     gen_login  
@@ -105,17 +122,11 @@ function main() {
     #lock_init system
     # here add your own commands and functions! 
     gen_sudo
-    place_bin test4me
     make_autostart
 
-echo "Fuer Graphische Oberflaeche:"
-echo "Settings -> User Auomatic login [enable]"
 }
 
 # Invoke main with args if not sourced
 if ! (return 0 2> /dev/null); then
     main "$@"
 fi
-
-
-
