148024e520a40a6a433c1364d95e1247e7d6af39
Andreas Schockenhoff <asc@ekf.de>
2023-06-05 07:30:54 +0200

    remote script with bug fix

--- remote.ps1
+++ remote.ps1
@@ -1,3 +1,5 @@
 $IP_REMOTE="172.21.120.76"
 
-ssh root@$IP_REMOTE "sed -e 's/ekf ALL = ALL/ /' /etc/sudors > /tmp/sudoers && mv /tmp/sudoers /etc/sudoers ; echo nameserver 172.21.130.2 > /etc/resolv.conf ; cp -r .ssh /home/ekf/ && cd /home/ekf ; chown -R ekf.ekf .ssh && git clone https://github.com/asc4asc/bash-script-template.git"
+ssh -o StrictHostKeyChecking=no root@$IP_REMOTE "sed -e  's/ekf   ALL = ALL/ /' /etc/sudoers > /tmp/sudoers && mv /tmp/sudoers /etc/sudoers ; echo nameserver 172.21.130.2 > /etc/resolv.conf ; cp -r .ssh /home/ekf/ && cd /home/ekf ; chown -R ekf.ekf .ssh" 
+ssh -o StrictHostKeyChecking=no ekf@$IP_REMOTE "git clone https://github.com/asc4asc/bash-script-template.git"
+ssh -o StrictHostKeyChecking=no ekf@$IP_REMOTE 
