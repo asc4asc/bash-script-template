3a98d65b8b35fe969305b894df558dfdd808b44c
Andreas Schockenhoff <asc@ekf.de>
2023-06-06 13:31:28 +0200

    add the function by name feature

--- getopts-template.sh
+++ getopts-template.sh
@@ -40,6 +40,34 @@ function demo_function() {
     echo "Hi, demo_function in action!"
 }
 
+# DESC: Generic script initialisation
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: $orig_cwd: The current working directory when the script was run
+#       $script_path: The full path to the script
+#       $script_dir: The directory path of the script
+#       $script_name: The file name of the script
+#       $script_params: The original parameters provided to the script
+#       $ta_none: The ANSI control code to reset all text attributes
+# NOTE: $script_path only contains the path that was used to call the script
+#       and will not resolve any symlinks which may be present in the path.
+#       You can use a tool like realpath to obtain the "true" path. The same
+#       caveat applies to both the $script_dir and $script_name variables.
+# shellcheck disable=SC2034
+function script_init() {
+    # Useful variables
+    readonly orig_cwd="$PWD"
+    readonly script_params="$*"
+    readonly script_path="${BASH_SOURCE[0]}"
+    script_dir="$(dirname "$script_path")"
+    script_name="$(basename "$script_path")"
+    script_ext="${script_name##*-}"
+    readonly script_ext script_dir script_name
+
+    # Important to always set as we use it in the exit handler
+    # shellcheck disable=SC2155
+    readonly ta_none="$(tput sgr0 2> /dev/null || true)"
+}
+
 # DESC: Usage help
 # ARGS: None
 # OUTS: None
@@ -85,11 +113,21 @@ function parse_params() {
 # OUTS: None
 function main() {
 
+    script_init
     parse_params "$@"
     #lock_init system
     # here add your own commands and functions!
     verbose_print "Show the verbose function!"
     [ ${demoflag} ] && demo_function
+    
+    if [ ${script_ext} == "reboot" ] 
+    then
+      echo ${script_ext};
+    else
+      echo No known script extension ${script_ext}  
+    fi
+    
+    script_usage 
 }
 
 # Invoke main with args if not sourced
