aa168793791dedaf25724f67589744aad81aa12d
Andreas Schockenhoff <asc@ekf.de>
2023-06-30 10:11:35 +0200

    scripts for debian live improve and working

--- live-term.bash
+++ live-term.bash
@@ -3,6 +3,7 @@ readonly TDIR=live-term
 # readonly export ARCHIVE_AREAS="main non-free non-free-firmware firmware-misc-non-free contrib" # does not help
 
 mkdir -p ${TDIR}/config/includes.chroot/usr/local/sbin
+mkdir -p ${TDIR}/config/includes.chroot/usr/local/bin
 cp progspi ${TDIR}/config/includes.chroot/usr/local/sbin
 cp PC7UEFI.BIN ${TDIR}/config/includes.chroot/usr/local/
 cp SC5UEFI.BIN ${TDIR}/config/includes.chroot/usr/local/
@@ -15,13 +16,36 @@ cat > ${TDIR}/config/includes.chroot/etc/skel/progsc5 << EOF
 sudo progspi /usr/local/SC5UEFI.BIN
 EOF
 
+cat > ${TDIR}/config/includes.chroot/usr/local/bin/prog4ekf << 'EOF'
+board=$(sudo dmidecode -s baseboard-product-name) 
+sudo progspi /usr/local/${board%-*}UEFI.BIN
+EOF
+
+cat > ${TDIR}/config/includes.chroot/etc/skel/.bashrc << 'EOF'
+board=$(sudo dmidecode -s baseboard-product-name) 
+echo "Interrupt with <CTRL C>" 
+echo "This is: "${board} 
+echo "Info: "${board%-*} 
+# sleep 10
+# prog4ekf
+# sudo reboot
+EOF
+
+chmod a+x ${TDIR}/config/includes.chroot/usr/local/sbin/* 
+chmod a+x ${TDIR}/config/includes.chroot/usr/local/bin/* 
 chmod a+x ${TDIR}/config/includes.chroot/etc/skel/progpc7 
 chmod a+x ${TDIR}/config/includes.chroot/etc/skel/progsc5 
 
+echo libc6-i386 > ${TDIR}/config/package-lists/ekf.list.chroot # needed for progsi ....
+
+#mkdir -p ${TDIR}/config/includes.chroot/etc/default/
+#cp /etc/default/grub ${TDIR}/config/includes.chroot/etc/default/
+
 # ( mkdir -p $TDIR && cd $TDIR && lb clean && lb config --archive-areas "main non-free contrib") # install bullseye
-( mkdir -p $TDIR && cd $TDIR && lb clean && lb config --mirror-bootstrap http://deb.debian.org/debian/ --mirror-binary http://deb.debian.org/debian/ --security false --updates false --distribution bookworm --debian-installer live --debian-installer-distribution bookworm --cache-packages false --archive-areas "main non-free-firmware contrib" )
-echo libc6-i386 >> ${TDIR}/config/package-lists/installer.list.chroot
-echo debian-installer-launcher >> ${TDIR}/config/package-lists/installer.list.chroot
+( mkdir -p $TDIR && cd $TDIR && lb clean && lb config --mirror-bootstrap "http://deb.debian.org/debian/" --mirror-binary "http://deb.debian.org/debian/" --security false --updates "false" --distribution "bookworm" --debian-installer "live" --debian-installer-distribution "bookworm" --cache-packages "false" --archive-areas "main non-free-firmware contrib" --bootappend-live "boot=live username=ekf toram" )
+#( mkdir -p $TDIR && cd $TDIR && lb clean && lb config --mirror-bootstrap "http://deb.debian.org/debian/" --mirror-binary "http://deb.debian.org/debian/" --security false --updates "false" --distribution "bookworm" --debian-installer "live" --debian-installer-distribution "bookworm" --cache-packages "false" --archive-areas "main non-free-firmware contrib" )
+# echo more-packages >> ${TDIR}/config/package-lists/ekf.list.chroot # add
+# echo debian-installer-launcher >> ${TDIR}/config/package-lists/installer.list.chroot
 
 ( cd ${TDIR} && lb build )
 
