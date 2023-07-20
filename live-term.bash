c3d65bc53929bb97d49954b167e74ca3687b1ffa
Andreas Schockenhoff <asc@ekf.de>
2023-07-20 15:03:56 +0200

    try more overview with function

--- live-term.bash
+++ live-term.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 readonly DISTRI="stable" # "bookworm" "trixie" "bullseye" (oldoldstable oldstable stable testing sid)
-readonly X="gnome" # "gnome","" terminal ohne X 
+readonly X="" # terminal ohne X 
+# readonly X="gnome" # "gnome","" terminal ohne X 
 readonly BOOT="toram" # "toram","" normal from usb ...
 # readonly FUNC="iperf-master" # iperf-master,"" no special function / not so easy to implement without confusing?
 
@@ -9,7 +10,15 @@ readonly BOOT="toram" # "toram","" normal from usb ...
 # [[ ${FUNC} == "iperf-master" ]] && readonly STARTIPERF=true || readonly STARTIPERF=false
 #-------------------------------------------------------
 
-function make-autostart-entry4gnome
+function repair-sudo4ekf-after-install
+{
+cat > ${TDIR}/config/includes.chroot/usr/local/bin/sudo4ekf << "EOF"
+echo 'ekf ALL=(ALL:ALL) NOPASSWD:ALL' > /tmp/ekf4testing && sudo cp /tmp/ekf4testing /etc/sudoers.d
+EOF
+sudo chmod a+x  ${TDIR}/config/includes.chroot/usr/local/bin/*
+}
+
+function cat-autostart-entry4gnome
 {
 cat > ${TDIR}/config/includes.chroot/etc/skel/.config/autostart/term.desktop << EOF
 [Desktop Entry]
@@ -26,73 +35,76 @@ Comment[en_US]=
 Comment=
 X-GNOME-Autostart-Delay=0
 EOF
+return 0
 }
 
 function auto-start4gnome
 {
-if ${GNOME}; then 
   mkdir -p ${TDIR}/config/includes.chroot/etc/skel/.config/autostart
-  make-autostart-entry4gnome
-else
-  mkdir -p ${TDIR}/config/includes.chroot/etc/skel
-fi
+  cat-autostart-entry4gnome
+  mkdir -p ${TDIR}/config/includes.chroot/etc/skel/.config
+  echo "yes" > ${TDIR}/config/includes.chroot/etc/skel/.config/gnome-initial-setup-done 
+  return 0
+}
+
+function setup-calamares-installer
+{
+  mkdir -p ${TDIR}/config/includes.chroot/etc/calamares
+  cp -r calamares/* ${TDIR}/config/includes.chroot/etc/calamares/
+}
+
+function live-config
+{
+( mkdir -p $TDIR && cd $TDIR && lb clean && lb config --mirror-bootstrap "http://deb.debian.org/debian/" --mirror-binary "http://deb.debian.org/debian/" --security false --updates "false" --distribution ${DISTRI} --cache-packages "false" --archive-areas "main non-free-firmware contrib" --bootappend-live "boot=live username=ekf ${BOOT}" )
+# ( mkdir -p $TDIR && cd $TDIR && lb clean && lb config --mirror-bootstrap "http://deb.debian.org/debian/" --mirror-binary "http://deb.debian.org/debian/" --security false --updates "false" --distribution "bookworm" --debian-installer "live" --debian-installer-distribution "bookworm" --cache-packages "false" --archive-areas "main non-free-firmware contrib" --bootappend-live "boot=live username=ekf toram" )
 return 0
 }
 
-# readonly TDIR="live-${FUNC}-${X}4term-${BOOT}-${DISTRI}"
-readonly TDIR="live-${X}4term-${BOOT}-${DISTRI}"
 
-#echo ${GNOME}
-#${GNOME} && echo ja
-#${GNOME} || echo nein
-# exit 0
-
-# ( mkdir -p $TDIR && cd $TDIR && lb clean && lb config --mirror-bootstrap "http://deb.debian.org/debian/" --mirror-binary "http://deb.debian.org/debian/" --security false --updates "false" --distribution "bookworm" --debian-installer "live" --debian-installer-distribution "bookworm" --cache-packages "false" --archive-areas "main non-free-firmware contrib" --bootappend-live "boot=live username=ekf toram" )
-( mkdir -p $TDIR && cd $TDIR && lb clean && lb config --mirror-bootstrap "http://deb.debian.org/debian/" --mirror-binary "http://deb.debian.org/debian/" --security false --updates "false" --distribution ${DISTRI} --cache-packages "false" --archive-areas "main non-free-firmware contrib" --bootappend-live "boot=live username=ekf ${BOOT}" )
-
-${GNOME} && echo task-gnome-desktop >> ${TDIR}/config/package-lists/desktop.list.chroot
-${GNOME} && echo calamares-settings-debian calamares >> ${TDIR}/config/package-lists/installer.list.chroot
-echo chrony >> ${TDIR}/config/package-lists/time.list.chroot
-
-auto-start4gnome 
-
-echo iperf3 >> ${TDIR}/config/package-lists/iperf.list.chroot
-
-if ${GNOME}; then 
-mkdir -p ${TDIR}/config/includes.chroot/etc/skel/autostart.dir
+function cat-autostart-network
+{
 cat > ${TDIR}/config/includes.chroot/etc/skel/autostart.dir/100-network << 'EOF'
 #!/bin/bash
 ip addr | grep "inet " | grep -v host
 iperf3 -s
 EOF
+}
 
-chmod a+x ${TDIR}/config/includes.chroot/etc/skel/autostart.dir/100-network
+function add-iperf3-4gnome
+{
+  echo iperf3 >> ${TDIR}/config/package-lists/iperf.list.chroot
 
-mkdir -p ${TDIR}/config/includes.chroot/etc/calamares
-cp -r calamares/* ${TDIR}/config/includes.chroot/etc/calamares/
-fi
+  mkdir -p ${TDIR}/config/includes.chroot/etc/skel/autostart.dir
+  cat-autostart-network
+  chmod a+x ${TDIR}/config/includes.chroot/etc/skel/autostart.dir/100-network
+}
+
+# readonly TDIR="live-${FUNC}-${X}4term-${BOOT}-${DISTRI}"
+readonly TDIR="live-${X}4term-${BOOT}-${DISTRI}"
+
+live-config
+
+${GNOME} && echo task-gnome-desktop >> ${TDIR}/config/package-lists/desktop.list.chroot
+${GNOME} && echo calamares-settings-debian calamares >> ${TDIR}/config/package-lists/installer.list.chroot
+echo chrony >> ${TDIR}/config/package-lists/time.list.chroot
+
+${GNOME} && auto-start4gnome 
+${GNOME} && add-iperf3-4gnome
+${GNOME} && setup-calamares-installer
 
 # cp -r /usr/share/live/build/bootloaders/* ${TDIR}/config/bootloaders/
 # cd  ${TDIR}/config/bootloaders
 # echo "Modify timeout in all bootloaders." 
 # bash
 cp -r bootloaders/* ${TDIR}/config/bootloaders/
+mkdir -p ${TDIR}/config/includes.chroot/etc/skel
 
+function prog4ekf-misc
+{
 mkdir -p ${TDIR}/config/includes.chroot/usr/local/sbin
 mkdir -p ${TDIR}/config/includes.chroot/usr/local/bin
-cp progspi ${TDIR}/config/includes.chroot/usr/local/sbin
-# cp PC7UEFI.BIN ${TDIR}/config/includes.chroot/usr/local/
-# cp SC5UEFI.BIN ${TDIR}/config/includes.chroot/usr/local/
+cp sbin4ekf/* ${TDIR}/config/includes.chroot/usr/local/sbin
 cp ekfuefi/* ${TDIR}/config/includes.chroot/usr/local/
-
-
-#mkdir -p ${TDIR}/config/includes.chroot/etc/skel/
-#cat > ${TDIR}/config/includes.chroot/etc/skel/progpc7 << EOF
-#sudo progspi /usr/local/PC7UEFI.BIN
-#EOF
-#cat > ${TDIR}/config/includes.chroot/etc/skel/progsc5 << EOF
-#sudo progspi /usr/local/SC5UEFI.BIN
-#EOF
 
 cat > ${TDIR}/config/includes.chroot/usr/local/bin/prog4ekf << 'EOF'
 board=$(sudo dmidecode -s baseboard-product-name) 
@@ -113,26 +125,42 @@ case ${bk} in
 esac
 exit 0
 EOF
+sudo chmod a+x  ${TDIR}/config/includes.chroot/usr/local/sbin/*
+sudo chmod a+x  ${TDIR}/config/includes.chroot/usr/local/bin/*
+}
+prog4ekf-misc
 
-cat > ${TDIR}/config/includes.chroot/etc/skel/.bash_history << 'EOF'
+function gen-hist-misc
+{
+cat >> ${TDIR}/config/includes.chroot/etc/skel/.bash_history << "EOF"
+gsettings set org.gnome.SessionManager logout-prompt 'false'
+sudo4ekf
+sudo calamares 
+EOF
+}
+gen-hist-misc
+
+function add2hist-efiboot-check
+{
+cat >> ${TDIR}/config/includes.chroot/etc/skel/.bash_history << "EOF"
+sudo efibootmgr --label ekf4nvme --create --disk=/dev/nvme0n1p1 --part=1 --loader='EFI\debian\grubx64'
+sudo efibootmgr --label deb4sda  --create --disk=/dev/sda --part=1 --loader='EFI\debian\grubx64.efi'
+EOF
+}
+add2hist-efiboot-check
+
+function add2history-prog
+{
+cat >> ${TDIR}/config/includes.chroot/etc/skel/.bash_history << "EOF"
 history
 sudo poweroff
 sudo prog4ekf
 EOF
-# sudo4ekf
-# sudo calamares 
+}
+add2history-prog
 
-if ${GNOME}; then 
-mkdir -p ${TDIR}/config/includes.chroot/etc/skel/.config
-cat > ${TDIR}/config/includes.chroot/etc/skel/.config/gnome-initial-setup-done << 'EOF'
-yes
-EOF
-fi
-
-cat > ${TDIR}/config/includes.chroot/usr/local/bin/sudo4ekf << "EOF"
-echo 'ekf ALL=(ALL:ALL) NOPASSWD:ALL' > /tmp/ekf4testing && sudo cp /tmp/ekf4testing /etc/sudoers.d
-EOF
-
+function auto-info-start-with-bashrc
+{
 cat > ${TDIR}/config/includes.chroot/etc/skel/.bashrc << 'EOF' # 
 board=$(sudo dmidecode -s baseboard-product-name) 
 #echo "Interrupt with <CTRL C>" 
@@ -140,27 +168,17 @@ echo "This is: "${board}
 echo "To update the UEFI BIOS please call: prog4ekf" 
 echo "After this please call: sudo poweroff"
 EOF
+}
 
-#echo "Info: "${board%-*} 
-# sleep 30
-# prog4ekf
+auto-info-start-with-bashrc
+
 # sudo reboot
 # sudo rtcwake -s 80 -m off  
-# EOF
-
-chmod a+x ${TDIR}/config/includes.chroot/usr/local/sbin/* 
-chmod a+x ${TDIR}/config/includes.chroot/usr/local/bin/* 
-# chmod a+x ${TDIR}/config/includes.chroot/etc/skel/progpc7 
-# chmod a+x ${TDIR}/config/includes.chroot/etc/skel/progsc5 
-
-# mkdir -p ${TDIR}/config/includes.chroot/etc/default/
-# cp /etc/default/grub ${TDIR}/config/includes.chroot/etc/default/
-
-# echo more-packages >> ${TDIR}/config/package-lists/ekf.list.chroot # add
-# echo debian-installer-launcher >> ${TDIR}/config/package-lists/installer.list.chroot
 
 echo libc6-i386 > ${TDIR}/config/package-lists/ekf.list.chroot # needed for progsi ....
-echo pciutils usbutils > ${TDIR}/config/package-lists/debug.list.chroot # needed for progsi ....
+echo efibootmgr pciutils usbutils > ${TDIR}/config/package-lists/debug.list.chroot # needed for progsi ....
+
+repair-sudo4ekf-after-install
 
 ( cd ${TDIR} && lb build )
 
